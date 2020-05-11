ad073666d80a1a53bf77cc097cb16da1f925d120
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-11 15:19:18 -0400

    Print input as bytes

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON); // disable echo and canonical
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+	enableRawMode();
+
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1);
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
