ddb462944aebfc70071599cc3d854bc6cb9be56f
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-11 15:52:45 -0400

    Full raw mode

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -6,29 +7,52 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+		die("tcsetattr");
+	}
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+		die("tcsetattr");
+	}
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON); // disable echo and canonical
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Ctrl-m, Ctrl-s, Ctrl-q
+	raw.c_oflag &= ~(OPOST); // disable carriage return
+	raw.c_cflag |= CS8;
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // disable echo and canonical, Ctrl-v/o, SIGINT/SIGTSTP
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die("tcsetattr");
+	}
 }
 
 int main() {
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		if (iscntrl(c)) {
-			printf("%d\n", c);
-		} else {
-			printf("%d ('%c')\n", c, c);
+	for (;;) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			die("read");
 		}
+
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+		if (c == 'q') break;
 	}
+
 	return 0;
 }
