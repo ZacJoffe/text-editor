b84dc1cafb6ce16eb38186f3cb676a9c151dfe04
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-13 00:16:40 -0400

    Refactors and blank screen

--- main.c
+++ main.c
@@ -5,9 +5,14 @@
 #include <unistd.h>
 #include <termios.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -37,21 +42,52 @@ void enableRawMode() {
 	}
 }
 
+char editorReadKey() {
+	int nread;
+	char c;
+
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) {
+			die("read");
+		}
+	}
+
+	return c;
+}
+
+void editorDrawRows() {
+	for (int y = 0; y < 24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+char editorProcessKeypress() {
+	char c = editorReadKey();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+			break;
+	}
+}
+
 int main() {
 	enableRawMode();
 
 	for (;;) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-			die("read");
-		}
-
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 
 	return 0;
