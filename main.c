d9be21ccd782ae1aabe1d622aecfe6f1fe98ddd9
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-14 00:18:56 -0400

    Tildes

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
+// includes
 #include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
+// defines
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+// data
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 void die(const char *s) {
 	write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -18,18 +28,18 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
 		die("tcsetattr");
 	}
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
 		die("tcsetattr");
 	}
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Ctrl-m, Ctrl-s, Ctrl-q
 	raw.c_oflag &= ~(OPOST); // disable carriage return
 	raw.c_cflag |= CS8;
@@ -55,9 +65,61 @@ char editorReadKey() {
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+		return -1;
+	}
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+			break;
+		}
+		if (buf[i] == 'R') {
+			break;
+		}
+		++i;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[') {
+		return -1;
+	}
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+		return -1;
+	}
+
+	
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+	struct winsize ws;
+	
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+			return -1;
+		}
+
+		return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+// output
+
 void editorDrawRows() {
-	for (int y = 0; y < 24; y++) {
-		write(STDOUT_FILENO, "~\r\n", 3);
+	for (int y = 0; y < E.screenrows; ++y) {
+		write(STDOUT_FILENO, "~", 1);
+
+		if (y < E.screenrows - 1) {
+			write(STDOUT_FILENO, "\r\n", 2);
+		}
 	}
 }
 
@@ -82,8 +144,17 @@ char editorProcessKeypress() {
 	}
 }
 
+// init
+
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+		die("getWindowSize");
+	}
+}
+
 int main() {
 	enableRawMode();
+	initEditor();
 
 	for (;;) {
 		editorRefreshScreen();
