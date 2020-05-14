08119eb7286ad9d2143c022504728695a94e1c9b
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-14 11:49:08 -0400

    Retab

--- main.c
+++ main.c
@@ -12,154 +12,154 @@
 
 // data
 struct editorConfig {
-	int screenrows;
-	int screencols;
-	struct termios orig_termios;
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
 };
 
 struct editorConfig E;
 
 void die(const char *s) {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 
-	perror(s);
-	exit(1);
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
-		die("tcsetattr");
-	}
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
-		die("tcsetattr");
-	}
-	atexit(disableRawMode);
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
+        die("tcsetattr");
+    }
+    atexit(disableRawMode);
 
-	struct termios raw = E.orig_termios;
-	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Ctrl-m, Ctrl-s, Ctrl-q
-	raw.c_oflag &= ~(OPOST); // disable carriage return
-	raw.c_cflag |= CS8;
-	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // disable echo and canonical, Ctrl-v/o, SIGINT/SIGTSTP
-	raw.c_cc[VMIN] = 0;
-	raw.c_cc[VTIME] = 1;
+    struct termios raw = E.orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Ctrl-m, Ctrl-s, Ctrl-q
+    raw.c_oflag &= ~(OPOST); // disable carriage return
+    raw.c_cflag |= CS8;
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // disable echo and canonical, Ctrl-v/o, SIGINT/SIGTSTP
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
-		die("tcsetattr");
-	}
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
 char editorReadKey() {
-	int nread;
-	char c;
+    int nread;
+    char c;
 
-	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-		if (nread == -1 && errno != EAGAIN) {
-			die("read");
-		}
-	}
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        }
+    }
 
-	return c;
+    return c;
 }
 
 int getCursorPosition(int *rows, int *cols) {
-  	char buf[32];
-	unsigned int i = 0;
+    char buf[32];
+    unsigned int i = 0;
 
-	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
-		return -1;
-	}
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+        return -1;
+    }
 
-	while (i < sizeof(buf) - 1) {
-		if (read(STDIN_FILENO, &buf[i], 1) != 1) {
-			break;
-		}
-		if (buf[i] == 'R') {
-			break;
-		}
-		++i;
-	}
-	buf[i] = '\0';
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+            break;
+        }
+        if (buf[i] == 'R') {
+            break;
+        }
+        ++i;
+    }
+    buf[i] = '\0';
 
-	if (buf[0] != '\x1b' || buf[1] != '[') {
-		return -1;
-	}
-	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
-		return -1;
-	}
+    if (buf[0] != '\x1b' || buf[1] != '[') {
+        return -1;
+    }
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+        return -1;
+    }
 
-	
-	return 0;
+    
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
-	struct winsize ws;
-	
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
-			return -1;
-		}
+    struct winsize ws;
+    
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+            return -1;
+        }
 
-		return getCursorPosition(rows, cols);
-	} else {
-		*cols = ws.ws_col;
-		*rows = ws.ws_row;
-		return 0;
-	}
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 // output
 
 void editorDrawRows() {
-	for (int y = 0; y < E.screenrows; ++y) {
-		write(STDOUT_FILENO, "~", 1);
+    for (int y = 0; y < E.screenrows; ++y) {
+        write(STDOUT_FILENO, "~", 1);
 
-		if (y < E.screenrows - 1) {
-			write(STDOUT_FILENO, "\r\n", 2);
-		}
-	}
+        if (y < E.screenrows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
 }
 
 void editorRefreshScreen() {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 
-	editorDrawRows();
+    editorDrawRows();
 
-	write(STDOUT_FILENO, "\x1b[H", 3);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 char editorProcessKeypress() {
-	char c = editorReadKey();
+    char c = editorReadKey();
 
-	switch (c) {
-		case CTRL_KEY('q'):
-			write(STDOUT_FILENO, "\x1b[2J", 4);
-			write(STDOUT_FILENO, "\x1b[H", 3);
-			exit(0);
-			break;
-	}
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
 }
 
 // init
 
 void initEditor() {
-	if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
-		die("getWindowSize");
-	}
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+        die("getWindowSize");
+    }
 }
 
 int main() {
-	enableRawMode();
-	initEditor();
+    enableRawMode();
+    initEditor();
 
-	for (;;) {
-		editorRefreshScreen();
-		editorProcessKeypress();
-	}
+    for (;;) {
+        editorRefreshScreen();
+        editorProcessKeypress();
+    }
 
-	return 0;
+    return 0;
 }
